beb991f2a0518ebe3b7db2b2c7ac4356065ca937
lihuanpeng <huanpenglee@gmail.com>
2017-03-16 14:22:14 +0800

    增加了编译poppler的脚本

--- zsh/debian.sh
+++ zsh/debian.sh
@@ -1,12 +1,24 @@
 # make mouse direction axis
-xmodmap -e "pointer = 1 2 3 5 4"
+# xmodmap -e "pointer = 1 2 3 5 4"
+
+# develop env
+export QTVERSION=4
 
 # those two lines for CUDA environment
 PATH="/usr/local/cuda-8.0/bin:${PATH}"
 LD_LIBRARY_PATH="/usr/local/cuda/lib64:${LD_LIBRARY_PATH}"
+
 # for Qt env
-PATH="/opt/Qt5.8.0/5.8/gcc_64/bin:${PATH}"
-LD_LIBRARY_PATH="/opt/Qt5.8.0/5.8/gcc_64/lib:${LD_LIBRARY_PATH}"
+if [ ${QTVERSION} = 4 ]; then
+    export QTDIR="/usr/local/Qt-4.8.5"
+    PATH="${QTDIR}/bin:${PATH}"
+    LD_LIBRARY_PATH="${QTDIR}/lib:${LD_LIBRARY_PATH}"
+elif [ ${QTVERSION} = 5 ]; then
+    export QTDIR="/opt/Qt5.8.0/5.8/gcc_64"
+    PATH="${QTDIR}/bin:${PATH}"
+    LD_LIBRARY_PATH="${QTDIR}/lib:${LD_LIBRARY_PATH}"
+fi
+
 # for java
 export JAVA_HOME="/usr/lib/jvm/jdk1.8.0_121"
 export JRE_HOME="${JAVA_HOME}/jre"
